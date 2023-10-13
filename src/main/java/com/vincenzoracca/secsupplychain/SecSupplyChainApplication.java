c7f5f324e2274ac5e7dece02e8d116d88bc25c8f
vincenzo-racca-pa <vincenzo.racca@pagopa.it>
2023-10-13 22:54:14 +0200

    first commit

---
+++ src/main/java/com/vincenzoracca/secsupplychain/SecSupplyChainApplication.java
@@ -0,0 +1,13 @@
+package com.vincenzoracca.secsupplychain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecSupplyChainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecSupplyChainApplication.class, args);
+	}
+
+}
